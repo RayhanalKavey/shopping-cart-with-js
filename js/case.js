6ebf76da25bfcebb9b4640d66705a5c76782207f
RayhanalKavey <rayhanalkavey@gmail.com>
2022-08-19 16:54:23 +0600

    make - button for phone case active. Validation for negative value, because we cannot order negative number of case.

--- js/case.js
+++ js/case.js
@@ -5,3 +5,14 @@ document.getElementById("btn-case-plus").addEventListener("click", function () {
   const newCaseNumber = previousCaseNumber + 1;
   setInputValueByID("case-number-field", newCaseNumber);
 });
+//Phone case - button
+document
+  .getElementById("btn-case-minus")
+  .addEventListener("click", function () {
+    const previousCaseNumber = getInputValueByID("case-number-field");
+    if (previousCaseNumber <= 0) {
+      return;
+    }
+    const newCaseNumber = previousCaseNumber - 1;
+    setInputValueByID("case-number-field", newCaseNumber);
+  });
